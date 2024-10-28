6d546fe3c431e67235e3eb47d8eb25eab861089c
oohmie <ohmzaz.b@gmail.com>
2024-10-28 22:04:45 +0700

    bug fix practice

--- scripts/front/debug_label.gd
+++ scripts/front/debug_label.gd
@@ -4,5 +4,5 @@ extends Label
 func _process(delta: float) -> void:
 	text = "your_turn: " + str(%GameManager.your_turn) + "\ngame_time: " + str(%GameManager.game_time)\
 	 + "\nmodal_time: "+ str(%GameManager.modal_time) + "\nfinal_time: "+ str(%GameManager.final_time)\
-	 + "\nis_clicked: " + str(%GameManager.is_clicked) + "\ndiff_score" + str(%GameManager.diff_score)\
-	+ "\ndiff_equation" + str(%GameManager.diff_equation)
+	 + "\nis_clicked: " + str(%GameManager.is_clicked) + "\ndiff_score" + str(%GameManager.diff_score)
+	#+ "\ndiff_equation" + str(%GameManager.diff_equation)
